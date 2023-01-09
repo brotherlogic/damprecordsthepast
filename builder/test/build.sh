1450e25e1dd20c6fae0f48f79886f26363a35ca8
Simon Tucker <brotherlogic@gmail.com>
2023-01-09 11:37:34 -0800

    Update the test (#5)

--- builder/test/build.sh
+++ builder/test/build.sh
@@ -2,6 +2,6 @@
 for i in {1..16}
 do
     echo "Downloading $i"
-    sleep 5
+    sleep 10
     curl -s "https://api.discogs.com/artists/2228/releases?page=$i" -o "https___api.discogs.com_artists_2228_releases_page=$i"
 done
