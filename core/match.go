b5f182debeed8a1f459c233b0ca1d6590628d3a5
Simon Tucker <brotherlogic@gmail.com>
2023-03-12 10:36:08 -0700

    Fixes build

--- core/match.go
+++ core/match.go
@@ -4,15 +4,17 @@ import (
 	pb "github.com/brotherlogic/damprecordsthepast/proto"
 )
 
-func ComputeMatch(user *pb.User, matcher *pb.Matcher) float64 {
+func ComputeMatch(user *pb.User, matcher *pb.Matcher) (float64, []*pb.Release) {
 	matched := float64(0)
 	count := float64(0)
 
+	var unmatched []*pb.Release
+
 	for _, entry := range matcher.GetMatches() {
 		found := false
-		for _, id := range entry.GetReleaseId() {
+		for _, id := range entry.GetRelease() {
 			for _, release := range user.GetOwnedReleases() {
-				if release == id {
+				if release == id.GetId() {
 					found = true
 					break
 				}
@@ -21,9 +23,11 @@ func ComputeMatch(user *pb.User, matcher *pb.Matcher) float64 {
 
 		if found {
 			matched++
+		} else {
+			unmatched = append(unmatched, entry.GetRelease()...)
 		}
 		count++
 	}
 
-	return 100 * (matched / count)
+	return 100 * (matched / count), unmatched
 }
